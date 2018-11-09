b77c55bc1ac40fbfdcc89a523a8abb1570b0a7b3
Gøran Sveia Kvarv <gkv@dips.no>
2018-11-09 09:02:32 +0100

    initial commit

---
+++ run.ps1
@@ -0,0 +1,10 @@
+function Exec([scriptblock]$cmd, [string]$errorMessage = "Error executing command: " + $cmd) {
+    & $cmd
+    if ($LastExitCode -ne 0) {
+        throw $errorMessage
+    }
+}
+
+exec { & npm install }
+
+exec { & newman run ./src/EhrStore.postman_collection.json }
