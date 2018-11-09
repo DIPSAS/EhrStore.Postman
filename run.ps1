b0d80587eca647b1eb9e77783e79623029221656
Bjørn Fjukstad <bfj@dips.no>
2018-11-09 14:04:44 +0100

    tests run against vt-selecta-srv

--- run.ps1
+++ run.ps1
@@ -6,8 +6,9 @@ function Exec([scriptblock]$cmd, [string]$errorMessage = "Error executing comman
 }
 exec { & npm install -g newman newman-reporter-teamcity }
 
+# Run newman tests for all json files named *collection.json in the src/ folder
 exec { &  Get-ChildItem -Path "src\*collection.json" -Recurse | Sort-Object Length -Descending | ForEach-Object {
-    newman run $_.FullName -r teamcity
-}
+    newman run $_.FullName  --global-var "Protocol=https" --global-var "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var "BasePath=openehr"  -k -r teamcity
+    }
 }
 
