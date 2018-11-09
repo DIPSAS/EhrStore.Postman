fdda56f3019076c25cc3bb62f7092ae620290372
Bjørn Fjukstad <bfj@dips.no>
2018-11-09 14:28:19 +0100

    update collections & run against multiple ehrstores

--- run.ps1
+++ run.ps1
@@ -9,6 +9,7 @@ exec { & npm install -g newman newman-reporter-teamcity }
 # Run newman tests for all json files named *collection.json in the src/ folder
 exec { &  Get-ChildItem -Path "src\*collection.json" -Recurse | Sort-Object Length -Descending | ForEach-Object {
     newman run $_.FullName  --global-var "Protocol=https" --global-var "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var "BasePath=openehr"  -k -r teamcity
+    newman run $_.FullName  --global-var "Protocol=https" --global-var "ServerHostname=vt-etl-selecta.etl.dips.no" --global-var "ServerPort=4443" --global-var "BasePath=openehr"  -k -r teamcity
     }
 }
 
