c33745f67db2eea49da7f1983f5122739d204001
Bjørn Fjukstad <bfj@dips.no>
2019-01-29 11:09:55 +0100

    fix: commandline params

--- run.ps1
+++ run.ps1
@@ -8,9 +8,7 @@ exec { & npm install -g newman newman-reporter-teamcity }
 
 # Run newman tests for all json files named *collection.json in the src/ folder
 exec { &  Get-ChildItem -Path "src\*collection.json" -Recurse | Sort-Object Length -Descending | ForEach-Object {
-    newman run $_.FullName  --global-var "Protocol=https" --global-var
-    "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var
-    "BasePath=openehr" -k -n 10 -r teamcity,cli
+    newman run $_.FullName  --global-var "Protocol=https" --global-var "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var "BasePath=openehr" -k -n 10 -r teamcity,cli
     }
 }
 
