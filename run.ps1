4caaad73c457fee2dd551ec266f4bc480d7c3d5c
Bjørn Fjukstad <bfj@dips.no>
2019-01-29 11:04:06 +0100

    experiment w/ running 10 iterations of api tests

--- run.ps1
+++ run.ps1
@@ -8,7 +8,9 @@ exec { & npm install -g newman newman-reporter-teamcity }
 
 # Run newman tests for all json files named *collection.json in the src/ folder
 exec { &  Get-ChildItem -Path "src\*collection.json" -Recurse | Sort-Object Length -Descending | ForEach-Object {
-    newman run $_.FullName  --global-var "Protocol=https" --global-var "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var "BasePath=openehr"  -k -r teamcity,cli
+    newman run $_.FullName  --global-var "Protocol=https" --global-var
+    "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var
+    "BasePath=openehr" -n 10 -k -r teamcity,cli
     }
 }
 
