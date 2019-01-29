a2992cd08a76a010c0dd5308403ea3d344d669ec
Bjørn Fjukstad <bfj@dips.no>
2019-01-29 11:05:55 +0100

    experiment w/ number of iterations

--- run.ps1
+++ run.ps1
@@ -10,7 +10,7 @@ exec { & npm install -g newman newman-reporter-teamcity }
 exec { &  Get-ChildItem -Path "src\*collection.json" -Recurse | Sort-Object Length -Descending | ForEach-Object {
     newman run $_.FullName  --global-var "Protocol=https" --global-var
     "ServerHostname=vt-selecta-srv" --global-var "ServerPort=4443" --global-var
-    "BasePath=openehr" -n 10 -k -r teamcity,cli
+    "BasePath=openehr" -k -n 10 -r teamcity,cli
     }
 }
 
